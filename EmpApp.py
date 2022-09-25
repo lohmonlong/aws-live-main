11cb7bdbbe55b3a6df008f59315789dac7a2f717
lohmonlong <112379506+lohmonlong@users.noreply.github.com>
2022-09-25 16:17:52 +0800

    up

--- EmpApp.py
+++ EmpApp.py
@@ -243,7 +243,7 @@ def applyLeave():
     reason = request.form['reason']
     status = "Approved"
 
-    emp_name = "Apply Name: " + str(name)
+    emp_name = "Apply Name:" + name
 
     insert_leave = ("INSERT INTO leave VALUES (%s,%s,%s,%s,%s,%s)")
     cursor = db_conn.cursor()
@@ -270,7 +270,7 @@ def applyLeave():
         cursor.close()
     
     print("Successfully Applied")
-    return render_template('LeaveAppOutput.html', name=str(emp_name))
+    return render_template('LeaveAppOutput.html', name=emp_name)
 
 
 if __name__ == '__main__':
